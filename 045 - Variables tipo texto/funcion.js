a6fb660bbb1b6030ff1265a99d7011bb455c8aca
Horckun <horckun@gmail.com>
2018-11-27 10:54:13 +0100

    045
    Variables tipo texto

---
+++ 045 - Variables tipo texto/funcion.js
@@ -0,0 +1,8 @@
+// Insertar comillas con barra invertida y salto de línea "" \n
+var texto1="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat\".\n\n"
+
+// Combinar comillas simples con dobles '' ""
+var texto2="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. \'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat\'."
+
+// Crear alerta
+alert(texto1+texto2);

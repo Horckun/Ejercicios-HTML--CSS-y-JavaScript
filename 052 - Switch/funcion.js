ae0740e8bebd8f24f7f49fde33a6b11edbd4bb6b
Horckun <horckun@gmail.com>
2018-12-10 10:57:23 +0100

    052
    Switch

---
+++ 052 - Switch/funcion.js
@@ -0,0 +1,17 @@
+// Declarar variable
+var num = 8;
+// Menú de selección
+switch (num) {
+  case 2:
+    alert("El valor es menor de 5.");
+    break;
+  case 5:
+    alert("El valor es 5.");
+    break;
+  case 8:
+    alert("El valor es mayor de 5.");
+    break;
+  default:
+    alert("No se ha detectado ningún número.");
+    break;
+}

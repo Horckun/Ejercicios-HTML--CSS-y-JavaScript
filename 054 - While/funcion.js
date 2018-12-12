2288a866178d747fa72d4a1c2ce46b8225b95dd8
Horckun <horckun@gmail.com>
2018-12-12 10:06:24 +0100

    054
    Bucle While

---
+++ 054 - While/funcion.js
@@ -0,0 +1,11 @@
+// Escribimos título
+document.write("<h2>Bucle While</h2>");
+
+// Declaramos variable
+var x = 0;
+
+// Buclle While
+while(x <= 8) {
+  document.write("Número: "+x+".<br>");
+  x++;
+}

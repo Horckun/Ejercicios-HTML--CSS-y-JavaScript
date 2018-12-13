11d34e46882c4d74141b7d433c65be1840ab960c
Horckun <horckun@gmail.com>
2018-12-13 21:27:09 +0100

    055
    Do while

---
+++ 055 - Do while/funcion.js
@@ -0,0 +1,13 @@
+// Declaramos variable
+var x = 8;
+var numero;
+
+// Solicitamos número
+do {
+  numero = prompt("Introduce un valor entre el 0 y el 10:");
+} while(numero != 8);
+
+// Verificamos número
+if(numero == x) {
+  alert("Has acertado");
+}

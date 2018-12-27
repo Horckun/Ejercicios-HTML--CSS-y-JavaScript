e41ca11dbd424a2e0c0f10265e75a4d9992eba72
Horckun <horckun@gmail.com>
2018-12-27 20:41:37 +0100

    063
    Métodos del objeto location

---
+++ 063 - Métodos del objeto location/funcion.js
@@ -0,0 +1,7 @@
+function funcion() {
+  // Forzar recarga de la página actual
+  location.reload();
+
+  // Cambiar URL incluso en el historial del navegador
+  location.replace("http://horckun.es");
+}

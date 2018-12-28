c6e735272f8ab2e605ff576091ea941e83e7c127
Horckun <horckun@gmail.com>
2018-12-28 11:51:45 +0100

    063
    Propiedades del objeto screen

---
+++ 063 - Propiedades del objeto screen/funcion.js
@@ -0,0 +1,21 @@
+function funcion() {
+  // Alto del navegador
+  var x = "Altura del navegador: "+screen.availHeight+"px<br>";
+  document.write(x);
+
+  // Ancho del navegador
+  var x = "Anchura del navegador: "+screen.availWidth+"px<br>";
+  document.write(x);
+
+  // Ancho total de la pantalla
+  var x = "Anchura del navegador: "+screen.width+"px<br>";
+  document.write(x);
+
+  // Alto total de la pantalla
+  var x = "Anchura del navegador: "+screen.height+"px<br>";
+  document.write(x);
+
+  // Número de bits por px utilizado
+  var x = "Anchura del navegador: "+screen.colorDepth+"<br>";
+  document.write(x);
+}

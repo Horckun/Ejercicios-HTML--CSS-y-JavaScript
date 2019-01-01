3bf6baf521b01f2228de8eb6a82010fa4c0840e4
Horckun <horckun@gmail.com>
2019-01-01 17:50:21 +0100

    064
    Propiedades del objeto navigator

---
+++ 064 - Porpiedades del objeto navigator/funcion.js
@@ -0,0 +1,42 @@
+// Función principal
+function funcion() {
+  // Nombre del código del navegador
+  var x = navigator.appCodeName;
+  document.write("Nombre del código del navegador: "+x+"<br>");
+
+  // Nombre del navegador
+  var x = navigator.appName;
+  document.write("Nombre del navegador: "+x+"<br>");
+
+  // Versión del navegador
+  var x = navigator.appVersion;
+  document.write("Versión del navegador: "+x+"<br>");
+
+  // Idioma del navegador
+  var x = navigator.language;
+  document.write("Idioma del navegador: "+x+"<br>");
+
+  // Sistema Operativo del navegador
+  var x = navigator.platform;
+  document.write("Sistema Operativo del navegador: "+x+"<br>");
+
+  // Plugins del navegador
+  var x = navigator.plugins;
+  document.write("Plugins del navegador: "+x+"<br>");
+
+  // Tipos de MIME del navegador
+  var x = navigator.mimeTypes;
+  document.write("Tipos de MIME del navegador: "+x+"<br>");
+
+  // Información del navegador
+  var x = navigator.userAgent;
+  document.write("Información del navegador: "+x+"<br>");
+
+  // Cookies del navegador
+  var x = navigator.cookieEnabled;
+  document.write("Cookies del navegador: "+x+"<br>");
+
+  // Cookies del navegador
+  var x = navigator.javaEnabled;
+  document.write("Cookies del navegador: "+x+"<br>");
+}

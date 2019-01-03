f5c2690a8ed72b328c1f0d15ece84bf5ae61ddd4
Horckun <horckun@gmail.com>
2019-01-03 14:04:45 +0100

    066
    Métodos del objeto history

---
+++ 066 - Métodos del objeto history/funcion.js
@@ -0,0 +1,10 @@
+function funcion() {
+  // Cargar la página anterior
+  history.back();
+
+  // Cargar la página siguiente
+  history.forward();
+
+  //Cargar la página indicada entre paréntesis
+  history.go(-8);
+}

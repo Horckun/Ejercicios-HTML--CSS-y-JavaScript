ad55e0d0ba0955a48335b768bda0a3394da7deb3
Horckun <horckun@gmail.com>
2019-01-08 17:49:27 +0100

    069
    Propiedades del objeto string

---
+++ 069 - Propiedades del objeto string/funcion.js
@@ -0,0 +1,6 @@
+function funcion() {
+  // Contador de número de carácteres
+  var frase = "Pero no hay queja del que deja y no se enoja por las tejas que caen del tejado agotadas por viejas"
+  var contar = frase.length;
+  document.write("La frase: "+frase+" tiene "+contar+" carácteres.");
+}

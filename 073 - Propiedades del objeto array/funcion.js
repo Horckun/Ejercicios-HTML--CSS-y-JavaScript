bafa48b32158df13ce849bb259514bbedbd587a8
Horckun <horckun@gmail.com>
2019-01-14 11:37:53 +0100

    073
    Propiedades del objeto Array

---
+++ 073 - Propiedades del objeto array/funcion.js
@@ -0,0 +1,8 @@
+function funcion() {
+  // Creación de Array
+  var cadena = new Array(8);
+  // Medición de Array
+  var tamano = cadena.length;
+  // Mostrar array
+  document.write("El array tiene "+tamano+" elemento");
+}

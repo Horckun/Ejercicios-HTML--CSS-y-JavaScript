e6a309e1e256d9506e09c070261b7c0cf44f7b8f
Horckun <horckun@gmail.com>
2019-01-23 14:07:11 +0100

    080
    Formularios y JavaScript

---
+++ 080 - Formularios y JavaScript/funcion.js
@@ -0,0 +1,17 @@
+// ESPAÑOL: Declarar variables
+// ENGLISH: Declare variables
+// FRANÇAIS: Déclarer des variables
+var nombre = datosPersonales.nombre.value;
+var apellidos = datosPersonales.apellidos.value;
+var comentarios = datosPersonales.comentarios.value;
+// ESPAÑOL: Declarar variables
+// ENGLISH: Function creation and data insertion
+// FRANÇAIS: Création de fonction et insertion de données
+function funcion() {
+  nombre = "Horckun";
+  document.write("Nombre: " + nombre + ".<br>");
+  apellidos = "Fercaso";
+  document.write("Apellidos: " + apellidos + ".<br>");
+  comentarios = "Síguenos";
+  document.write("Comentarios: " + comentarios + ".<br>");
+}

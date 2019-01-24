dcaa202ae13eddbcc65b7746e3d4109ba9ae7138
Horckun <horckun@gmail.com>
2019-01-24 14:42:31 +0100

    081
    Propiedades de los objetos de tipo texto

---
+++ 081 - Propiedades de los objetos de tipo texto/funcion.js
@@ -0,0 +1,24 @@
+// ESPAÑOL: Declarar variables
+// ENGLISH: Declare variables
+// FRANÇAIS: Déclarer des variables
+
+// Devuelve valor por defecto | Returns default value | Renvoie la valeur par défaut
+var nombre = datosPersonales.nombre.defaultValue;
+// Indica a qué formulario pertenece | Indicate to which form it belongs | Indiquez à quelle forme il appartient
+var apellidos = datosPersonales.apellidos.datosPersonales;
+// Indica a qué id pertenece | Indicate to which id it belongs | Indiquer à quel id il appartient
+var comentarios = datosPersonales.comentarios.id;
+// Indica si está activo o no (true o false) | Indicates whether it is active or not (true or false) | Indique s'il est actif ou non (vrai ou faux)
+var comentarios = datosPersonales.comentarios.disabled;
+
+// ESPAÑOL: Crear función e inserción de variables
+// ENGLISH: Function creation and data insertion
+// FRANÇAIS: Création de fonction et insertion de données
+function funcion() {
+  nombre = "Horckun";
+  document.write("Nombre: " + nombre + ".<br>");
+  apellidos = "Fercaso";
+  document.write("Apellidos: " + apellidos + ".<br>");
+  comentarios = "Síguenos";
+  document.write("Comentarios: " + comentarios + ".<br>");
+}

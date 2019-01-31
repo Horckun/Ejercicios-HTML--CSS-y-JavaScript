af327560ae42aff091d447495b6dcc99cd11b55b
Horckun <horckun@gmail.com>
2019-01-31 14:50:12 +0100

    086
    Propiedades de los checkbox

---
+++ 086 - Propiedades de los checkbox/funcion.js
@@ -0,0 +1,9 @@
+// Nos dice si está activada o no | Tells us if it is activated or not | Nous dit si c'est activé ou pas
+var caja1 = datosPersonales.caja1.checked;
+// Valor del elemento | Element value | Valeur de l'élément
+var caja2 = datosPersonales.caja2.value;
+
+function funcion() {
+  document.write("Caja 1: " + caja1 + ".<br>");
+  document.write("Caja 2: " + caja2 + ".<br>");
+}

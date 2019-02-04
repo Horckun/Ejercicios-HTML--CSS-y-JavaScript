3b1de6d65f5621f8a66d1dafe7fa04db0fc49855
Horckun <horckun@gmail.com>
2019-02-04 15:45:59 +0100

    089
    Ejercicio1

---
+++ 087 - Métodos de los checkbox/funcion.js
@@ -0,0 +1,9 @@
+// Simula hacer clic | Simulates click | Simule le clic
+var caja1 = datosPersonales.caja1.click();
+document.write("Caja 1: " + caja1 + ".<br>");
+// Perder y dar foco | Losing and giving focus | Perdre et donner le focus
+var caja1 = datosPersonales.caja2.blur();
+var caja2 = datosPersonales.caja2.focus();
+
+document.write("Caja 1: " + caja1 + ".<br>");
+document.write("Caja 2: " + caja2 + ".<br>");
---
+++ 088 - Botones de opción o radio button/funcion.js
@@ -0,0 +1,7 @@
+// Radio length, check and value
+var radio = datosPersonales.profesion.length;
+document.write("Radio: " + radio + ".<br>");
+var radio = datosPersonales.profesion.[1].checked;
+document.write("Radio: " + radio + ".<br>");
+var radio = datosPersonales.profesion.[1].value;
+document.write("Radio: " + radio + ".<br>");

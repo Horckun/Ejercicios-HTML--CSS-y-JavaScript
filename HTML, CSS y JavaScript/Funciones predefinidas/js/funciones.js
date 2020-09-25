8461ce99433e8aac617c643ad39d8fb29c6691c1
Horckun <horckun@gmail.com>
2020-09-25 11:01:14 +0200

    Funciones predefinidas
    Funciones predefinidas en JavaScript

--- HTML, CSS y JavaScript/Funciones predefinidas/js/funciones.js
+++ HTML, CSS y JavaScript/Funciones predefinidas/js/funciones.js
@@ -2,3 +2,9 @@
 var evaluar = escape("Horckun: informática y programación.");
 // MOSTRAR EN PANTALLA
 document.write(evaluar);
+//----------------------
+document.write("<hr>");
+// DECLARAR VARIABLE
+var evaluar = unescape("Horckun%3A%20inform%E1tica%20y%20programaci%F3n.");
+// MOSTRAR EN PANTALLA
+document.write(evaluar);

6966952e5f952eb13fb085c74b3a19f16ff5f729
Horckun <horckun@gmail.com>
2020-10-08 13:16:14 +0200

    Propiedades del objeto window
    Propiedades del objeto window en JavaScript

--- HTML, CSS y JavaScript/Propiedades del objeto window/js/funciones.js
+++ HTML, CSS y JavaScript/Propiedades del objeto window/js/funciones.js
@@ -6,3 +6,15 @@ if(window.close() == 0) {
   // SI SE HA CERRADO MUESTRA ADIÓS
   alert("adiós");
 }
+//------------------------------------------------
+// VER HISTORIAL
+// ver cuántas páginas se han visitado anteriormente
+//var historial = window.history.length;
+// ver página anterior
+//var historial = window.history.back();
+// ver página siguiente
+//var historial = window.history.forward();
+// abrir una página específica
+var historial = window.history.go();
+// alerta
+alert(historial);

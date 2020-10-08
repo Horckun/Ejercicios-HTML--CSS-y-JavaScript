6552c383fa0ab636fab995c42a7be13d921d57de
Horckun <horckun@gmail.com>
2020-10-08 13:14:27 +0200

    Propiedades del objeto window
    Propiedades del objeto window en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades del objeto window/js/funciones.js
@@ -0,0 +1,8 @@
+// COMPROBAR SI UNA VENTANA SE HA CERRADO O NO
+if(window.close() == 0) {
+  // SI SE HA CERRADO MUESTRA HOLA
+  alert("hola")
+} else {
+  // SI SE HA CERRADO MUESTRA ADIÓS
+  alert("adiós");
+}

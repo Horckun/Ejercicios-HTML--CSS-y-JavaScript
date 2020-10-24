85e548b0d88cc5298f5520f39448eabff2a1e7e6
Horckun <horckun@gmail.com>
2020-10-24 12:10:21 +0200

    Métodos del objeto window
    Métodos del objeto window en JavaScript

--- HTML, CSS y JavaScript/Métodos del objeto window/js/funciones.js
+++ HTML, CSS y JavaScript/Métodos del objeto window/js/funciones.js
@@ -2,7 +2,17 @@
 // abrir web en una nueva pestaña
 window.open("https://horckun.es","Nueva pestaña");
 // ABRIR VENTANA NUEVA
-// abrir web en una nueva ventana con altura
-window.open("https://horckun.es","Ventana","heigth=100px");
-// abrir web en una nueva ventana con altura y anchura
-window.open("https://horckun.es","Ventana","heigth=100px,width=100px");
+// abrir web en una nueva ventana con altura y anchura específica
+window.open("https://horckun.es","Ventana","height=600,width=300");
+// DECLARAR VARIABLE GLOBAL
+var horckun;
+// CREAR FUNCIÓN
+function abrir() {
+  // abrir web en nueva ventana
+  horckun = window.open("https://horckun.es","Ventana","height=600,width=300");
+}
+// CREAR FUNCIÓN
+function cerrar() {
+  // cerrar web de nueva ventana
+  horckun.close();
+}

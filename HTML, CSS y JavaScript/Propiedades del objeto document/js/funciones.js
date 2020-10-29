6b6c5d53e174ef6ee9b0305ad564dc8914d09801
Horckun <horckun@gmail.com>
2020-10-29 11:08:01 +0100

    Propiedades del objeto document
    Propiedades del objeto document en JavaScript

--- HTML, CSS y JavaScript/Propiedades del objeto document/js/funciones.js
+++ HTML, CSS y JavaScript/Propiedades del objeto document/js/funciones.js
@@ -1,4 +1,7 @@
-// CAMBIAR COLOR DE FONDO
+// CAMBIAR ESTILOS
 function funcion() {
+  // cambiar color de fondo
   document.body.style.backgroundColor="#0043b6";
+  // cambiar color de fuente
+  document.fgColor="#eee";
 }

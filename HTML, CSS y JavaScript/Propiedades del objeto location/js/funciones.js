414a04b1786d5c6dacbc2c8289b611f7d2082933
Horckun <horckun@gmail.com>
2020-11-13 12:41:56 +0100

    Propiedades del objeto location
    Propiedades del objeto location en JavaScript

--- HTML, CSS y JavaScript/Propiedades del objeto location/js/funciones.js
+++ HTML, CSS y JavaScript/Propiedades del objeto location/js/funciones.js
@@ -1,3 +1,12 @@
 // MOSTRAR SERVIDOR
+// declarar variable y asignar valor
 var servidor = location.host;
+// mostrar variable
 document.write("<h1>Servidor: "+servidor+"</h1>");
+// SEPARACIÓN
+document.write("<hr>");
+// MOSTRAR DIRECCIÓN DE SERVIDOR
+// declarar variable y asignar valor
+var dir = location.hostname;
+// mostrar variable
+document.write("<h1>Dirección de servidor: "+dir+"</h1>");

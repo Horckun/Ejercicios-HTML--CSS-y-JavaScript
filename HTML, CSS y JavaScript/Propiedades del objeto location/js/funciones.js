1143fed1f1d19f1ac3c5c71bfe5c3d0a86ea779a
Horckun <horckun@gmail.com>
2020-11-16 11:28:21 +0100

    Propiedades del objeto document
    Propiedades del objeto document en JavaScript

--- HTML, CSS y JavaScript/Propiedades del objeto location/js/funciones.js
+++ HTML, CSS y JavaScript/Propiedades del objeto location/js/funciones.js
@@ -2,11 +2,18 @@
 // declarar variable y asignar valor
 var servidor = location.host;
 // mostrar variable
-document.write("<h1>Servidor: "+servidor+"</h1>");
+document.write("<h1>Nombre del servidor: "+servidor+"</h1>");
 // SEPARACIÓN
 document.write("<hr>");
 // MOSTRAR DIRECCIÓN DE SERVIDOR
 // declarar variable y asignar valor
 var dir = location.hostname;
 // mostrar variable
-document.write("<h1>Dirección de servidor: "+dir+"</h1>");
+document.write("<h1>Dirección del servidor: "+dir+"</h1>");
+// SEPARACIÓN
+document.write("<hr>");
+// MOSTRAR DIRECCIÓN DE SERVIDOR
+// declarar variable y asignar valor
+var puerto = location.port;
+// mostrar variable
+document.write("<h1>Puerto del servidor: "+puerto+"</h1>");

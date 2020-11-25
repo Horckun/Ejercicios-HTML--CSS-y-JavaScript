f89fef8eb1681e538d01671a2883263664b10597
Horckun <horckun@gmail.com>
2020-11-25 12:23:00 +0100

    Propiedades del objeto screen
    Propiedades del objeto screen en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades del objeto screen/js/funciones.js
@@ -0,0 +1,6 @@
+// AVERIGUAR ALTO DE PANTALLA DEL NAVEGADOR
+var altura = screen.availHeight;
+// mostrar en pantalla
+document.write("Altura: "+altura+"px.");
+// SEPARACIÓN
+document.write("<hr>");

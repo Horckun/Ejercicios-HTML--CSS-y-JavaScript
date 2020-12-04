1a453871e4d24c5310d5c1482cbf7e48db9c8549
Horckun <horckun@gmail.com>
2020-12-04 10:03:03 +0100

    Propieddades del objeto navigator
    Propieddades del objeto navigator en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades del objeto navigator/js/funciones.js
@@ -0,0 +1,6 @@
+// AVERIGUAR NOMBRE DEL CÓDIGO DEL NAVEGADOR
+var app = navigator.appCodeName;
+// mostrar en pantalla
+document.write("Código de navegador: "+app+".");
+// SEPARACIÓN
+document.write("<hr>");

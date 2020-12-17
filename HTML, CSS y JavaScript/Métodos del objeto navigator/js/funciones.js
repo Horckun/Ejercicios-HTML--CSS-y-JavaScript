87a615460e71788b65d6df5eb45128d52e5064da
Horckun <horckun@gmail.com>
2020-12-17 11:34:08 +0100

    Métodos del objeto navigator
    Métodos del objeto navigator en JavaScript

---
+++ HTML, CSS y JavaScript/Métodos del objeto navigator/js/funciones.js
@@ -0,0 +1,6 @@
+// AVERIGUAR SI JAVA ESTÁ ACTIVO EN EL NAVEGADOR
+var java = navigator.javaEnabled();
+// mostrar en pantalla
+document.write("Java: "+java+".");
+// SEPARACIÓN
+document.write("<hr>");

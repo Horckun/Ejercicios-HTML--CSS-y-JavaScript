08681242847d189d279d1fdd0141b4b06e0d313b
Horckun <horckun@gmail.com>
2020-12-18 11:44:11 +0100

    Propiedades del objeto history
    Propiedades del objeto history en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades del objeto history/js/funciones.js
@@ -0,0 +1,6 @@
+// AVERIGUAR SI JAVA ESTÁ ACTIVO EN EL NAVEGADOR
+var historial = history.length;
+// mostrar en pantalla
+document.write("Historial: "+historial+".");
+// SEPARACIÓN
+document.write("<hr>");

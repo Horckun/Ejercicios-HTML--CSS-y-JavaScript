2fc92dca0739449bb1d4743bf9a78e06975dc04a
Horckun <horckun@gmail.com>
2020-12-28 12:01:26 +0100

    Constructor de Date
    Constructor de Date en JavaScript

--- HTML, CSS y JavaScript/Constructor de Date/js/funciones.js
+++ HTML, CSS y JavaScript/Constructor de Date/js/funciones.js
@@ -1,3 +1,12 @@
 // VER FECHA Y HORA ACTUAL
 var fecha = new Date();
+// MOSTRAR EN PANTALLA
 document.write(fecha);
+// SALTO DE LÍNEA
+document.write("<hr>");
+// VER FECHA Y HORA ESPECÍFICA
+var fecha2 = new Date(1989,0,27);
+// MOSTRAR EN PANTALLA
+document.write(fecha2);
+// SALTO DE LÍNEA
+document.write("<hr>");

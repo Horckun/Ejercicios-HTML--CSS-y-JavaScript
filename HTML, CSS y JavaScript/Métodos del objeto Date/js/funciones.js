818c244fc8b499c8bbde31adbc4a85948952e1c8
Horckun <horckun@gmail.com>
2020-12-29 10:17:10 +0100

    Métodos del objeto Date
    Métodos del objeto Date en JavaScript

---
+++ HTML, CSS y JavaScript/Métodos del objeto Date/js/funciones.js
@@ -0,0 +1,9 @@
+// VER DÍA ACTUAL
+// coger fecha actual
+var fecha = new Date();
+// coger día de la fecha actual
+var dia = fecha.getDate();
+// MOSTRAR EN PANTALLA
+document.write("Día del mes: "+dia);
+// SALTO DE LÍNEA
+document.write("<hr>");

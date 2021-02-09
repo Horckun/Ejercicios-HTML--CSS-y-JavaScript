de10aa8dda30810be16aace6671913963e427d06
Horckun <horckun@gmail.com>
2021-02-09 10:19:07 +0100

    Propiedades del objeto String
    Propiedades del objeto String en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades del objeto String/js/funciones.js
@@ -0,0 +1,9 @@
+// VER LONGITUD DE UNA CADENA
+// declarar variable
+var cadena = "Pero no hay queja del que deja y no se enoja por las tejas que caen del tejado agotadas por viejas";
+// coger longitud de cadena
+var num = cadena.length;
+// MOSTRAR EN PANTALLA
+document.write("El string: <strong>"+cadena+"</strong> tiene "+num+" carácteres.");
+// SALTO DE LÍNEA
+document.write("<hr>");

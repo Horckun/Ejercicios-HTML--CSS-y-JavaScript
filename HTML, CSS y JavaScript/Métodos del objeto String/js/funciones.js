59d17a52526e242f194048554e63bb8b246d6ef3
Horckun <horckun@gmail.com>
2021-02-11 15:06:37 +0100

    Métodos del objeto String
    Métodos del objeto String en JavaScript

--- HTML, CSS y JavaScript/Métodos del objeto String/js/funciones.js
+++ HTML, CSS y JavaScript/Métodos del objeto String/js/funciones.js
@@ -2,8 +2,15 @@
 // declarar variables
 var cadena = "Pero no hay queja del que deja y no se enoja por las tejas que caen del tejado agotadas por viejas";
 // buscar palabra en la cadena
-var buscar = cadena.indexOf("tejas");
+var buscar = cadena.indexOf("no");
 // MOSTRAR EN PANTALLA
 document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>tejas</strong> se encuentra en la posición "+buscar+".");
+// SALTO DE LÍNEA
+document.write("<hr>");
+// BÚSQUEDA DE UNA CADENA
+// buscar palabra en la cadena el último resultado
+var buscarUl = cadena.lastIndexOf("no");
+// MOSTRAR EN PANTALLA
+document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>tejas</strong> se encuentra en la posición "+buscarUl+".");
 // SALTO DE LÍNEA
 document.write("<hr>");

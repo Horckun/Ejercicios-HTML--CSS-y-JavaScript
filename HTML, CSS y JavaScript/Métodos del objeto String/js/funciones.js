2a01412d8a8d5337a49e8f0247e20ef7ac1bcab8
Horckun <horckun@gmail.com>
2021-02-12 11:04:14 +0100

    Métodos del objeto String
    Métodos del objeto String en JavaScript

--- HTML, CSS y JavaScript/Métodos del objeto String/js/funciones.js
+++ HTML, CSS y JavaScript/Métodos del objeto String/js/funciones.js
@@ -4,13 +4,20 @@ var cadena = "Pero no hay queja del que deja y no se enoja por las tejas que cae
 // buscar palabra en la cadena
 var buscar = cadena.indexOf("no");
 // MOSTRAR EN PANTALLA
-document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>tejas</strong> se encuentra en la posición "+buscar+".");
+document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>no</strong> se encuentra en la posición "+buscar+".");
 // SALTO DE LÍNEA
 document.write("<hr>");
 // BÚSQUEDA DE UNA CADENA
 // buscar palabra en la cadena el último resultado
 var buscarUl = cadena.lastIndexOf("no");
 // MOSTRAR EN PANTALLA
-document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>tejas</strong> se encuentra en la posición "+buscarUl+".");
+document.write("En la cadena: <strong>"+cadena+"</strong>, la palabra <strong>no</strong> se encuentra en la posición "+buscarUl+".");
+// SALTO DE LÍNEA
+document.write("<hr>");
+// BÚSQUEDA ENTRE UNA CADENA
+// buscar carácteres en la cadena entre el inicio y fin
+var entre = cadena.substring(8,80);
+// MOSTRAR EN PANTALLA
+document.write("En la cadena: <strong>"+cadena+"</strong>, entre la posición <strong>8</strong> y <strong>80</strong> se encuentra: "+entre+".");
 // SALTO DE LÍNEA
 document.write("<hr>");

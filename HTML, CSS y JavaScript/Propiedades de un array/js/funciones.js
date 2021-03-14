d6f2b61db4137f24c68394965c79ce92f7ea556e
Horckun <horckun@gmail.com>
2021-03-14 19:43:39 +0100

    Propiedades de un array
    Propiedades de un array en JavaScript

---
+++ HTML, CSS y JavaScript/Propiedades de un array/js/funciones.js
@@ -0,0 +1,12 @@
+// CREAR ARRAY
+// declarar variable para crear un vector
+var vacio = new Array("Horckun","programador","informático");
+var i,contar;
+// asignar valor
+contar = 0;
+// bucle for
+for(i = 0; i < vacio.length; i++) {
+  contar++;
+}
+// mostrar en pantalla
+document.write("Se han encontrado "+contar+" resultados en el vector.");

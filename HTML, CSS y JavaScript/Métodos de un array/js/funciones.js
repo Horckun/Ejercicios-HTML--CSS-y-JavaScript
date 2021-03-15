2a4c77501df78c7f5bc6925dfca651e879542d6a
Horckun <horckun@gmail.com>
2021-03-15 10:35:40 +0100

    Métodos del objeto Array
    Métodos del objeto Array en JavaScript

---
+++ HTML, CSS y JavaScript/Métodos de un array/js/funciones.js
@@ -0,0 +1,8 @@
+// CREAR ARRAY
+// declarar variables y asignarle vectores
+var vector = new Array("Horckun","programador","informático");
+var vector2 = new Array(32,"Bordeaux")
+// declarar variable final y juntar las dos anteriores
+var vectorFinal = vector.concat(vector2);
+// mostrar en pantalla
+document.write("El vector final sería: "+vectorFinal+".");
